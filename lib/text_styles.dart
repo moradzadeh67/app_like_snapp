6f3e1b5e33ea417f0f2ae79d2013ecce8382c8ec
reza <moradzade.mohamadreza@gmail.com>
2025-06-30 23:11:16 -0700

    third commit

---
+++ lib/text_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTextStyles {
+  MyTextStyles._();
+
+  static TextStyle bottun = GoogleFonts.vazirmatn(fontSize: 20);
+}
