bcb38bc7609523b9c2ef6fa2d2e8d7f169beb1da
reza <moradzade.mohamadreza@gmail.com>
2025-07-09 23:44:21 -0700

    forth commit

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/destination.svg
+  String get destination => 'assets/icons/destination.svg';
+
+  /// File path: assets/icons/origin.svg
+  String get origin => 'assets/icons/origin.svg';
+
+  /// List of all assets
+  List<String> get values => [destination, origin];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,4 +1,7 @@
+import 'package:app_like_snapp/text_styles.dart';
 import 'package:flutter/material.dart';
+
+import 'dimens.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -12,7 +15,44 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Stack(children: [Container(color: Colors.green)]),
+        body: Stack(
+          children: [
+            Container(color: Colors.green),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('انتخاب مبدا', style: MyTextStyles.bottun),
+              ),
+            ),
+            Positioned(
+              top: Dimens.medium,
+              left: Dimens.medium,
+
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: const Offset(2, 3),
+                      blurRadius: 18,
+                    ),
+                  ],
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
